a3c4592c0486dbc08a8ead5c549d86e41dcf44ea
akui27 <pyaesoneminnaing2019@gmail.com>
2022-04-15 21:13:35 +0630

    commit

--- src/components/Sidebar/Latestitems/LatestitemsCard/LatestitemsCard.jsx
+++ src/components/Sidebar/Latestitems/LatestitemsCard/LatestitemsCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import {li1} from '../../../../img';
 
 const LatestitemsCard = () => {
   return (
@@ -8,7 +9,7 @@ const LatestitemsCard = () => {
             <div className='ichiban-ichiba__latest-items-card-content'>
                 <div className='ichiban-ichiba__latest-items-card-content__pic'>
                     <a href='#'>
-                        <img src="src/img/li1.jpg" alt='product'/>
+                        <img src={li1} alt='product'/>
                     </a>
                 </div>
             </div>
