81266484e64a98b60d7eb9b4679d6c64187d55f3
akui27 <pyaesoneminnaing2019@gmail.com>
2022-05-02 14:43:40 +0630

    2-5

---
+++ src/components/mini-components/BrandsArea/BrandsArea.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './styles.css'
+import {b1} from "../../../img"
+
+const BrandsArea = () => {
+  return (
+      <>
+        <div className='ichiban-ichiba__brands-area'>
+            <div className='ichiban-ichiba__brands-area__container'>
+                <div className='ichiban-ichiba__brands-area__layout'>
+                    <div className='ichiban-ichiba__brands-area__layout-row'>
+                        <div className='ichiban-ichiba__brands-area__layout-row__col'>
+                            <div className='ichiban-ichiba__brands-area__layout-row__col-items'>
+                                <div className='ichiban-ichiba__brands-area__layout-row__col-items__list'>
+                                    <div className='ichiban-ichiba__brands-area__layout-row__col-items__list-track'>
+                                        <div className='ichiban-ichiba__brands-area__layout-row__col-items__list-track__brands'>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <img src={b1} alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </>
+  )
+}
+
+export default BrandsArea
+
